0e37cde97ef1da3d0ee7c44207aaf42759820ccc
Beth Smith <bethsmith0623@gamil.com>
2019-09-09 14:47:52 -0500

    adds data to urls.py and views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse ('Welcome to Yorkie Collector!')
+
+def about(request):
+    return HttpResponse ('This is Beth\'s Yorkie Collector!')
