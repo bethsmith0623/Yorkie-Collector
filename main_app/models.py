84440225edf2fe79c26c853c6b2fc438665ea264
Beth Smith <bethsmith0623@gamil.com>
2019-09-09 19:03:54 -0500

    updates model and adds superuser

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Yorkie(models.Model):
     name = models.CharField(max_length=150)
+    gender = models.CharField(max_length=25, default='male')
+    fixed = models.BooleanField(default=False)
     sire = models.CharField(max_length=150)
     dame = models.CharField(max_length=150)
     description = models.TextField(max_length=300)
